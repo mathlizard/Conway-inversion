ef9d82c5e0df5ff6cc6877efa5a58071c66a2b09
Tamas Hajgato <mathlizard@users.noreply.github.com>
2017-06-02 03:22:38 +0200

    Update star_inverse.py
    mmmmm

--- star_inverse.py
+++ star_inverse.py
@@ -3,7 +3,7 @@ Computes the inverse of a matrix using the Conway star matrix formula.
 call it with `star_inverse(m)`
 where `m` is a (square) numpy matrix
 """
-import numpy
+import numpy as np
 from numpy import dot
 import time
 
@@ -11,8 +11,8 @@ import time
 def star(m, k=1):
     """ recursive way of computing star """
     if (m.shape == (1,1)):
-        return numpy.matrix([[1. / (1. - m[0,0])]])
-    r = numpy.zeros(shape=m.shape)
+        return np.matrix([[1. / (1. - m[0,0])]])
+    r = np.zeros(shape=m.shape)
     a_star = star(m[:k,:k])
     d_star = star(m[k:,k:])
     r[:k,:k] = star(m[:k,:k] + dot(dot(m[:k,k:], d_star), m[k:,:k]))
@@ -26,7 +26,7 @@ def star_inverse(m):
     """ returns the inverse of m """
     if (m.shape[0] != m.shape[1]):
         raise ValueError("m must be a square matrix! ")
-    return star(numpy.eye(m.shape[0]) - m)
+    return star(np.eye(m.shape[0]) - m)
 
 
 def test(m):
@@ -35,7 +35,7 @@ def test(m):
     print("-" * 80)
     print("matrix:")
     print(m)
-    ei = abs(numpy.linalg.eigvals(m))
+    ei = abs(np.linalg.eigvals(m))
     print("maximum absolute eigenvalue = " + str(max(ei)))
     print("minimum absolute eigenvalue = " + str(min(ei)))
     print()
@@ -47,7 +47,7 @@ def test(m):
     print("numpy.linalg.inv : ")
     try:
         t = time.time()
-        print(numpy.linalg.inv(m))
+        print(np.linalg.inv(m))
         print("time: " + str(time.time() - t))
     except:
         print("linalg: singular matrix")
@@ -55,10 +55,10 @@ def test(m):
 
 def main():
     """ call this to test the matrix inverse """
-    test(numpy.matrix([[1,2], [3,4]]))
-    test(numpy.matrix([[3,2,3], [4,5,3], [7,8,9]]))
-    test(numpy.matrix([[1,2,3], [4,5,6], [7,8,9]]))
-    test(numpy.matrix([[1,9,9], [10,1,3], [10,9,5]]))
+    test(np.matrix([[1,2], [3,4]]))
+    test(np.matrix([[3,2,3], [4,5,3], [7,8,9]]))
+    test(np.matrix([[1,2,3], [4,5,6], [7,8,9]]))
+    test(np.matrix([[1,9,9], [10,1,3], [10,9,5]]))
 
 
 if __name__ == '__main__':
